6cc87b4c3258082ff81b0d61a64f6327747f83cb
KevinMendieta <kjmendieta@resuelvetudeuda.com>
2019-09-01 01:52:26 -0500

    card ajusts

--- src/app/films-list/film-item/index.js
+++ src/app/films-list/film-item/index.js
@@ -3,9 +3,9 @@ import React from 'react'
 const FilmItem = ({name, description}) => {
   return(
     <li>
-      <p>{name}</p>
+      <div className='card-title'>{name}</div>
       <img src="https://img.moviepostershop.com/totoro-my-neighbor-movie-poster-1988-1010550311.jpg" alt="Boston cream pie."></img>
-      <p>{description}</p>
+      <p className='card-content'>{description}</p>
     </li>
   )
 }
--- src/app/films-list/index.js
+++ src/app/films-list/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import FilmItem from './film-item'
 const FILMS = [
-  {name: 'Totoro', description: 'bla-bla-bla', id: '123'},
-  {name: 'Adventures', description: 'bla-bla-bla', id: '1234'},
-  {name: 'Fireflies', description: 'bla-bla-bla', id: '1235'},
-  {name: 'Haxhi', description: 'bla-bla-bla', id: '1236'},
-  {name: 'Magician', description: 'bla-bla-bla', id: '1237'},
-  {name: 'Mononoque', description: 'bla-bla-bla', id: '1238'}
+  {name: 'Totoro', description: 'In the latter part of World War II, a boy and his sister, orphaned when their mother is killed in the firebombing of Tokyo, are left to survive on their own in what remains of civilian life in Japan. The plot follows this boy and his sister as they do their best to survive in the Japanese countryside, battling hunger, prejudice, and pride in their own quiet, personal battle.', id: '123'},
+  {name: 'Adventures', description: 'In the latter part of World War II, a boy and his sister, orphaned when their mother is killed in the firebombing of Tokyo, are left to survive on their own in what remains of civilian life in Japan. The plot follows this boy and his sister as they do their best to survive in the Japanese countryside, battling hunger, prejudice, and pride in their own quiet, personal battle.', id: '1234'},
+  {name: 'Fireflies', description: 'In the latter part of World War II, a boy and his sister, orphaned when their mother is killed in the firebombing of Tokyo, are left to survive on their own in what remains of civilian life in Japan. The plot follows this boy and his sister as they do their best to survive in the Japanese countryside, battling hunger, prejudice, and pride in their own quiet, personal battle.', id: '1235'},
+  {name: 'Haxhi', description: 'In the latter part of World War II, a boy and his sister, orphaned when their mother is killed in the firebombing of Tokyo, are left to survive on their own in what remains of civilian life in Japan. The plot follows this boy and his sister as they do their best to survive in the Japanese countryside, battling hunger, prejudice, and pride in their own quiet, personal battle.', id: '1236'},
+  {name: 'Magician', description: 'In the latter part of World War II, a boy and his sister, orphaned when their mother is killed in the firebombing of Tokyo, are left to survive on their own in what remains of civilian life in Japan. The plot follows this boy and his sister as they do their best to survive in the Japanese countryside, battling hunger, prejudice, and pride in their own quiet, personal battle.', id: '1237'},
+  {name: 'Mononoque', description: 'In the latter part of World War II, a boy and his sister, orphaned when their mother is killed in the firebombing of Tokyo, are left to survive on their own in what remains of civilian life in Japan. The plot follows this boy and his sister as they do their best to survive in the Japanese countryside, battling hunger, prejudice, and pride in their own quiet, personal battle.', id: '1238'}
 ]
 class FilmList extends React.Component {
   constructor(props) {
